23647da8bdf8d48b17fb9181d66b4e9f4ab62b78
adriancorro <adriancorrog@gmail.com>
2021-09-20 20:38:14 +0200

    make it better

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,8 @@ import './css/App.css';
 import './css/style.css';
 import './css/normalize.css';
 import './js/script.js';
-import {BrowserRouter, Route, Switch} from 'react-router-dom';
+import {BrowserRouter, Route, Switch ,HashRouter } from 'react-router-dom';
+
 import IndexMain from './pages/IndexMain.js';
 import NotFound from './pages/NotFound.js';
 import LoginForm from './components/LoginForm.js';
@@ -12,15 +13,13 @@ import LoginForm from './components/LoginForm.js';
 
 function App() {
   return (
-    <BrowserRouter>
-     
+    <HashRouter>
         <Switch>
             <Route exact path='/' component={IndexMain}/>
             <Route exact path='/login' component={LoginForm}/>
             <Route component={NotFound}/>
         </Switch>
-      
-    </BrowserRouter>
+   </HashRouter>
   );
 }
 
