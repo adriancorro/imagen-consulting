4cb7a6a5dbddebacf575366ff9b39f567f1e09ad
adriancorro <adriancorrog@gmail.com>
2021-09-20 22:22:42 +0200

    make it better

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,7 +4,7 @@ import './css/style.css';
 import './css/normalize.css';
 import './js/script.js';
 import {HashRouter, Route, Switch } from 'react-router-dom';
-
+ 
 import IndexMain from './pages/IndexMain.js';
 import NotFound from './pages/NotFound.js';
 import LoginForm from './components/LoginForm.js';
